ce028866576c496206d5c3db0e83fee498d59237
Jimmy Lam <dev.jimmylam@gmail.com>
2021-02-19 17:27:58 -0800

    added icon styles

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -11,13 +11,13 @@ const Header = () => {
 					<Navbar.Toggle aria-controls="basic-navbar-nav" />
 					<Navbar.Collapse id="basic-navbar-nav">
 						<Nav className="ml-auto">
-							<Nav.Link href="/cart">
-								<i className='fas fa-shopping-cart'></i>
-								Cart
+							<Nav.Link 
+							href="/cart"
+							className='mr-2'>
+								<i className='fas fa-shopping-cart px-2'></i>Cart
 							</Nav.Link>
 							<Nav.Link href="/login">
-								<i className='fas fa-user'></i>
-								Sign In
+								<i className='fas fa-user px-2'></i>Sign In
 							</Nav.Link>
 						</Nav>
 					</Navbar.Collapse>

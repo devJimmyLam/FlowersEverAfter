893e0698bef340c909dfc4254df2028aff7ff92b
Jimmy Lam <dev.jimmylam@gmail.com>
2021-02-19 16:57:43 -0800

    added react boostrap

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Container, Row, Column } from 'react-bootstrap';
+const Footer = () => {
+	return (
+		<footer>
+			<Container>
+				<Row>
+					<Col>
+					</Col>
+				</Row>
+			</Container>
+		</footer>
+	)
+}
+
+export default Footer;
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,13 @@
+//rafce
+import React from 'react'
+import { Container, Row, Column } from 'react-bootstrap';
+
+const Header = () => {
+	return (
+		<header>
+			Header
+		</header>
+	)
+}
+
+export default Header

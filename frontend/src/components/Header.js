19d5adf39c8d0b71ce9f1bb7b8d46ffe39c728db
Jimmy Lam <dev.jimmylam@gmail.com>
2021-02-19 17:12:45 -0800

    added navbar

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,11 +1,22 @@
 //rafce
-import React from 'react'
-
+import React from 'react';
+import { Navbar, Nav, Containter, Container } from 'react-bootstrap';
 
 const Header = () => {
 	return (
 		<header>
-			Header
+			<Navbar bg="dark" varient='dark' expand="lg" collapseOnSelect>
+				<Container>
+					<Navbar.Brand href="/">FlowersEverAfter</Navbar.Brand>
+					<Navbar.Toggle aria-controls="basic-navbar-nav" />
+					<Navbar.Collapse id="basic-navbar-nav">
+						<Nav className="ml-auto">
+							<Nav.Link href="/cart">Cart</Nav.Link>
+							<Nav.Link href="/login">Sign In</Nav.Link>
+						</Nav>
+					</Navbar.Collapse>
+				</Container>
+			</Navbar>
 		</header>
 	)
 }

df2f4ac391b2ed38e0b290e9bcf0f4c9b5343d6b
Jimmy Lam <dev.jimmylam@gmail.com>
2021-02-21 22:22:16 -0800

    added LinkContainer

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,24 +1,34 @@
 //rafce
-import React from 'react';
-import { Navbar, Nav, Containter, Container } from 'react-bootstrap';
+import React from 'react'
+import { LinkContainer } from 'react-router-bootstrap'
+import { Navbar, Nav, Containter, Container } from 'react-bootstrap'
 
 const Header = () => {
 	return (
 		<header>
 			<Navbar bg="dark" varient='dark' expand="lg" collapseOnSelect>
 				<Container>
-					<Navbar.Brand href="/">FlowersEverAfter</Navbar.Brand>
+					<LinkContainer to='/'>
+						<Navbar.Brand >FlowersEverAfter</Navbar.Brand>
+					</LinkContainer>
+
 					<Navbar.Toggle aria-controls="basic-navbar-nav" />
 					<Navbar.Collapse id="basic-navbar-nav">
 						<Nav className="ml-auto">
-							<Nav.Link 
-							href="/cart"
-							className='mr-2'>
-								<i className='fas fa-shopping-cart px-2'></i>Cart
-							</Nav.Link>
-							<Nav.Link href="/login">
-								<i className='fas fa-user px-2'></i>Sign In
-							</Nav.Link>
+
+							<LinkContainer to='/cart'>
+								<Nav.Link
+									className='mr-2'>
+									<i className='fas fa-shopping-cart px-2'></i>Cart
+								</Nav.Link>
+							</LinkContainer>
+
+							<LinkContainer to='/login'>
+								<Nav.Link>
+									<i className='fas fa-user px-2'></i>Sign In
+								</Nav.Link>
+							</LinkContainer>
+							
 						</Nav>
 					</Navbar.Collapse>
 				</Container>

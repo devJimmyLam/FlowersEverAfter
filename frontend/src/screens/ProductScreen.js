1ffd07505b5cf8b9d90c74c9366f4187c4d97c1b
Jimmy Lam <dev.jimmylam@gmail.com>
2021-02-24 23:00:03 -0800

    added useState and useEffect to fetch product from backend

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { Row, Col, Image, ListGroup, Card, Button, Form } from 'react-bootstrap'
 import Rating from '../components/Rating'
-import products from '../products.js'
+import axios from 'axios'
+
 
 const ProductScreen = ({ match }) => {
-	const product = products.find((p) => p._id === match.params.id)
+	const [product, setProduct] = useState({})
+
+	//allow us to fetch product from react 
+	useEffect(() => {
+		const fetchProduct = async () => {
+			const { data } = await axios.get(`api/products${match.params.id}`)
+
+			setProduct(data)
+		}
+		fetchProduct()
+	}, [])
 	return (
 		<div>
 			<Link
@@ -17,9 +28,9 @@ const ProductScreen = ({ match }) => {
 				<Col md={6}>
 					<Image src={product.image} alt={product.name} fluid />
 				</Col>
-				<Col 
-				md={3}
-				className='productDetail'
+				<Col
+					md={3}
+					className='productDetail'
 				>
 					<ListGroup.Item>
 						<h3>{product.name}</h3>

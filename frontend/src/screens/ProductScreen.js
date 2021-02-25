558d1d5bf6de2de64bedc40f6cdf1602bf7930e1
Jimmy Lam <dev.jimmylam@gmail.com>
2021-02-24 23:17:01 -0800

    added useState and useEffect to fetch product from backend

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -8,15 +8,16 @@ import axios from 'axios'
 const ProductScreen = ({ match }) => {
 	const [product, setProduct] = useState({})
 
-	//allow us to fetch product from react 
 	useEffect(() => {
 		const fetchProduct = async () => {
-			const { data } = await axios.get(`api/products${match.params.id}`)
+			const { data } = await axios.get(`/api/products/${match.params.id}`)
 
 			setProduct(data)
 		}
 		fetchProduct()
 	}, [])
+
+
 	return (
 		<div>
 			<Link
@@ -28,25 +29,25 @@ const ProductScreen = ({ match }) => {
 				<Col md={6}>
 					<Image src={product.image} alt={product.name} fluid />
 				</Col>
-				<Col
-					md={3}
-					className='productDetail'
-				>
-					<ListGroup.Item>
-						<h3>{product.name}</h3>
-					</ListGroup.Item>
-					<ListGroup.Item>
-						<Rating
-							value={product.rating}
-							text={`${product.numReviews} reviews`}
-						/>
-					</ListGroup.Item>
-					<ListGroup.Item>
-						Price: ${product.price}
-					</ListGroup.Item>
-					<ListGroup.Item>
-						Description: {product.description}
-					</ListGroup.Item>
+				<Col md={3}>
+					<ListGroup variant='flush'>
+						<ListGroup.Item>
+							<h3>{product.name}</h3>
+						</ListGroup.Item>
+						<ListGroup.Item>
+							<Rating
+								value={product.rating}
+								text={`${product.numReviews} reviews`}
+							/>
+						</ListGroup.Item>
+						<ListGroup.Item>
+							Price: ${product.price}
+						</ListGroup.Item>
+						<ListGroup.Item>
+							Description: {product.description}
+						</ListGroup.Item>
+					</ListGroup>
+
 				</Col>
 				<Col md={3}>
 					<Card>

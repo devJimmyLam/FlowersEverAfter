d3ed66c591990a1f34c3387fbca7cd442c5d009f
Jimmy Lam <dev.jimmylam@gmail.com>
2021-02-26 13:26:00 -0800

    added bcryptjs

---
+++ backend/data/users.js
@@ -0,0 +1,20 @@
+
+
+const users = [
+	{
+		name: 'Admin User',
+		email: 'admin@example.com',
+		password: xxxx,
+		isAdmin: true
+	},
+	{
+		name: 'Tony Stark',
+		email: 'tony@example.com',
+		password: xxxx,
+	},
+	{
+		name: 'Steve Rogers',
+		email: 'steve@example.com',
+		password: xxxx,
+	}
+]

f9f247dfd3cc008251bb15c028bbe4369a2f4e98
Jimmy Lam <dev.jimmylam@gmail.com>
2021-02-26 13:51:16 -0800

    fixed typo bcrypt.hashSync

--- backend/data/users.js
+++ backend/data/users.js
@@ -4,19 +4,19 @@ const users = [
 	{
 		name: 'Admin User',
 		email: 'admin@example.com',
-		password: bcrypt.hasSync('123456', 10),
+		password: bcrypt.hashSync('123456', 10),
 		isAdmin: true
 	},
 	{
 		name: 'Tony Stark',
 		email: 'tony@example.com',
-		password: bcrypt.hasSync('123456', 10),
+		password: bcrypt.hashSync('123456', 10),
 	},
 	{
 		name: 'Steve Rogers',
 		email: 'steve@example.com',
-		password: bcrypt.hasSync('123456', 10),
+		password: bcrypt.hashSync('123456', 10),
 	}
-]
+];
 
 export default users;

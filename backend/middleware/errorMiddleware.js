a550d58ade9bdf4adcce2f1a508a911a2b6e7f9c
Jimmy Lam <dev.jimmylam@gmail.com>
2021-02-26 21:18:45 -0800

    added middleware and custom error handling

---
+++ backend/middleware/errorMiddleware.js
@@ -0,0 +1,20 @@
+//errror handler = middleware: function that has access to the request response cycle.
+//when we make a request, we can have a function that can access in these objects
+
+
+const notFound = (req, res, next) => {
+	const error = new Error(`Not Found - ${req.originalUrl}`)
+	res.status(404)
+	next(error)
+}
+
+const errorHandler = (err, req, res, next) => {
+	const statusCode = res.statusCode === 200 ? 500 : res.statusCode
+	res.status(statusCode)
+	res.json({
+		message: err.message,
+		stack: process.env.NODE_ENV === 'production' ? null : err.stack,
+	})
+}
+
+export { notFound, errorHandler }

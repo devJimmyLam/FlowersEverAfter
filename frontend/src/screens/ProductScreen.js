7b93b0f7d4b4bb5701ed4c314460c9cd14d1a3ce
Jimmy Lam <dev.jimmylam@gmail.com>
2021-03-02 08:45:51 -0800

    rector code

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -96,14 +96,13 @@ const ProductScreen = ({ history, match }) => {
 													value={qty}
 													onChange={(e) => setQty(e.target.value)}
 												>
-													{[...Array(product.countInStock).keys()].map((x) => (
-														<option
-															key={x + 1}
-															value={x + 1}
-														>
-															{x + 1}
-														</option>
-													))}
+													{[...Array(product.countInStock).keys()].map(
+														(x) => (
+															<option key={x + 1} value={x + 1}>
+																{x + 1}
+															</option>
+														)
+													)}
 												</Form.Control>
 											</Col>
 										</Row>
